56027e26c2a6717ef80d64d8a869a04d8bd4c945
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2025-04-15 03:34:03 -0600

    feat(cors): adding cors handling and verify auth endpoint

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, UserApiView
+from .views import LoginView, VerifyAuthView, UserApiView
 
 urlpatterns = [
     path("auth/login", LoginView.as_view(), name="login"),
+    path("auth/verify", VerifyAuthView.as_view(), name="verify"),
     path("user", UserApiView.as_view(), name="crud_user"),
 ]
 
