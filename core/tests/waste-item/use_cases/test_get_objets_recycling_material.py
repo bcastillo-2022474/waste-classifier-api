fbd29ec3a953ec9ab9a61fc9e7433cf1e35d4c87
carlosaltan18 <carlosaltan850@gmail.com>
2025-05-07 23:14:17 -0600

    feat(use case get objets recycling material): Se agrego el use case para las stats del material

--- core/tests/waste-item/use_cases/test_get_objets_recycling_material.py
+++ core/tests/waste-item/use_cases/test_get_objets_recycling_material.py
@@ -9,40 +9,41 @@ class TestGetObjetsRecyclingMaterialUseCase(unittest.TestCase):
         # Crear un mock del repositorio
         self.mock_repository = MagicMock(spec=WasteItemRepository)
         self.use_case = GetObjetsRecyclingMaterialUseCase(self.mock_repository)
-
+    
     def test_execute_returns_material_and_count(self):
         # Configurar el mock para devolver un objeto StatsWasteItem
         material_waste = "Plastic"
         expected_item = StatsWasteItem(material=material_waste, count=10)
         self.mock_repository.getMaterial.return_value = expected_item
-
+        
         # Ejecutar el caso de uso
         result = self.use_case.execute(material_waste)
-
+        
         # Verificar que el repositorio fue llamado correctamente
         self.mock_repository.getMaterial.assert_called_once_with(material_waste)
-
+        
         # Verificar que el resultado sea el esperado
-        self.assertEqual(result, StatsWasteItem(material="Plastic", count=10))
-    def test_execute_returns_none_when_material_not_found(self):
+        self.assertEqual(result, {"material": "Plastic", "count": 10})
+    
+    def test_execute_returns_default_when_material_not_found(self):
         # Configurar el mock para devolver None
         material_waste = "UnknownMaterial"
         self.mock_repository.getMaterial.return_value = None
-
+        
         # Ejecutar el caso de uso
         result = self.use_case.execute(material_waste)
-
+        
         # Verificar que el repositorio fue llamado correctamente
         self.mock_repository.getMaterial.assert_called_once_with(material_waste)
-
-        # Verificar que el resultado sea None
-        self.assertIsNone(result)
-
+        
+        # Verificar que el resultado sea el valor predeterminado
+        self.assertEqual(result, {"material": "unknown", "count": 0})
+    
     def test_execute_raises_error_when_material_is_empty(self):
         # Ejecutar el caso de uso con un material vacío y verificar que lanza un ValueError
         with self.assertRaises(ValueError) as context:
             self.use_case.execute("")
-
+        
         # Verificar el mensaje de error
         self.assertEqual(str(context.exception), "El material no puede estar vacío.")
 
